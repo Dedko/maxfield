260e250896aa4906b67011dd33989bd6e8854b13
Dedko <d@we.org.ua>
2016-02-26 11:52:01 +0200

    Update iitc_maxfields_plugin.js

--- iitc_maxfields_plugin.js
+++ iitc_maxfields_plugin.js
@@ -2,7 +2,7 @@
 // @id             maxfields@dedko
 // @name           IITC plugin: Maxfields Export to CSV
 // @category       Keys
-// @version        1.0.0.0
+// @version        1.0.0.1
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @description    Exports portals currently in view as a CSV list for use with Maxfields python library (https://github.com/mvinni/maxfield).
 // @include        https://www.ingress.com/intel*
@@ -33,7 +33,7 @@ function wrapper() {
         var dialog = window.dialog({
             title: "Maxfield CSV export",
             html: '<span>Save the list below as a CSV file, in folder with maxfields library, then run<code>python maxfield.py EXAMPLE.csv</code> from terminal</span>'
-            + '<textarea id="idmCSVExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
+            + '<textarea id="CSVExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
         }).parent();
         $(".ui-dialog-buttonpane", dialog).remove();
         // width first, then centre
@@ -41,13 +41,13 @@ function wrapper() {
             "top": ($(window).height() - dialog.height()) / 2,
             "left": ($(window).width() - dialog.width()) / 2
         });
-        $("#idmCSVExport").val(o.join("\n"));
+        $("#CSVExport").val(o.join("\n"));
         return dialog;
     }
     // setup function called by IITC
     self.setup = function init() {
         // add controls to toolbox
-        var link = $("<a onclick=\"window.plugin.ingressdualmap.gen();\" title=\"Generate a CSV list of portals and locations for use with Ingress Dual Map.\">IDM Export</a>");
+        var link = $("<a onclick=\"window.plugin.ingressdualmap.gen();\" title=\"Generate a CSV list of portals and locations for use with maxfields python library.\">CSV Export</a>");
         $("#toolbox").append(link);
         // delete setup to ensure init can't be run again
         delete self.setup;
